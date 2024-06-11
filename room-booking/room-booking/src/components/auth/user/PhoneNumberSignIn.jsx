b05051f5f3a1ccdc44ed8918f8279b0461dfa92f
SAEED NM <saeedmoozhikkal@gmail.com>
2024-06-11 09:25:51 +0530

    after adding signin

--- room-booking/room-booking/src/components/auth/user/PhoneNumberSignIn.jsx
+++ room-booking/room-booking/src/components/auth/user/PhoneNumberSignIn.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import instance from "../../Axiox";
+import { CircularProgress } from "@mui/material"; // For loading spinner
+import { FaGoogle } from "react-icons/fa"; // For Google icon
 
 const PhoneNumberSignIn = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -7,20 +9,6 @@ const PhoneNumberSignIn = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [otpSent, setOtpSent] = useState(false);
-
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      if (event.key === "Enter" && !otpSent) {
-        handleOtpSending();
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyPress);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyPress);
-    };
-  }, [otpSent]);
 
   const handlePhoneNumberChange = (event) => {
     setPhoneNumber(event.target.value);
@@ -35,7 +23,17 @@ const PhoneNumberSignIn = () => {
     }
   };
 
+  const handleKeyDown = (event, index) => {
+    if (event.key === "Backspace" && index > 0 && !otp[index]) {
+      document.getElementById(`otp-${index - 1}`).focus();
+    }
+  };
+
   const handleOtpSending = async () => {
+    if (!phoneNumber) {
+      setError("Phone number cannot be empty.");
+      return;
+    }
     setLoading(true);
     setError("");
     try {
@@ -47,7 +45,9 @@ const PhoneNumberSignIn = () => {
       setOtpSent(true);
     } catch (error) {
       if (error.response && error.response.data) {
-        setError(error.response.data.Error || "Failed to send OTP. Please try again.");
+        setError(
+          error.response.data.Error || "Failed to send OTP. Please try again."
+        );
       } else {
         setError("Failed to send OTP. Please try again.");
       }
@@ -67,7 +67,9 @@ const PhoneNumberSignIn = () => {
       console.log("OTP resent successfully:", response.data);
     } catch (error) {
       if (error.response && error.response.data) {
-        setError(error.response.data.Error || "Failed to resend OTP. Please try again.");
+        setError(
+          error.response.data.Error || "Failed to resend OTP. Please try again."
+        );
       } else {
         setError("Failed to resend OTP. Please try again.");
       }
@@ -81,12 +83,18 @@ const PhoneNumberSignIn = () => {
     setLoading(true);
     setError("");
     try {
-      const otpString = otp.join(""); 
-      const response = await instance.post("accounts/verify-ph-otp/", { otp: otpString, phone_number: phoneNumber });
+      const otpString = otp.join("");
+      const response = await instance.post("accounts/verify-ph-otp/", {
+        otp: otpString,
+        phone_number: phoneNumber,
+      });
       console.log("OTP verification successful:", response.data);
+      alert('succes')
     } catch (error) {
       if (error.response && error.response.data) {
-        setError(error.response.data.Error || "Failed to verify OTP. Please try again.");
+        setError(
+          error.response.data.Error 
+        );
       } else {
         setError("Failed to verify OTP. Please try again.");
       }
@@ -97,72 +105,92 @@ const PhoneNumberSignIn = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8 bg-gray-200 rounded-md shadow-md">
-      <div className="mb-4">
-        <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
-          Phone Number:
-        </label>
-        <input
-          type="text"
-          placeholder="Enter Phone number"
-          className="mt-1 p-2 border rounded-md w-full"
-          value={phoneNumber}
-          onChange={handlePhoneNumberChange}
-        />
-      </div>
-      {otpSent && (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded-md shadow-md max-w-sm w-full">
+        <h2 className="text-center text-2xl font-semibold mb-4">Sign in</h2>
         <div className="mb-4">
-          <label htmlFor="otp" className="block text-sm font-medium text-gray-700">
-            Enter OTP:
+          <label
+            htmlFor="phone"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Phone Number:
           </label>
-          <div className="flex">
-            {[...Array(6)].map((_, index) => (
-              <input
-                key={index}
-                type="text"
-                id={`otp-${index}`}
-                className="w-12 h-12 p-2 border rounded-md text-center mx-1"
-                maxLength="1"
-                value={otp[index] || ""}
-                onChange={(e) => handleOtpChange(e, index)}
-                onKeyDown={(e) => {
-                  if (e.key === "Backspace" && !otp[index] && index > 0) {
-                    e.preventDefault();
-                    document.getElementById(`otp-${index - 1}`).focus();
-                  }
-                }}
-              />
-            ))}
+          <div className="flex items-center mt-1">
+            <div className="flex items-center border border-gray-300 rounded-l-md px-3 w-36 h-10">
+              <span className="text-gray-500">IND (+91)</span>
+            </div>
+            <div className="h-full w-px bg-gray-300"></div>
+            <input
+              type="text"
+              placeholder="Enter Your Number"
+              className="p-2 border rounded-r-md w-full"
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+            />
           </div>
         </div>
-      )}
-      {error && <p className="text-red-500">{error}</p>}
-      {!otpSent ? (
-                <button
-          onClick={handleOtpSending}
-          className={`bg-blue-500 text-white py-2 px-4 rounded-md ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-          disabled={loading}
-        >
-          {loading ? "Sending OTP..." : "Send OTP"}
-        </button>
-      ) : (
-        <div>
+        {otpSent && (
+          <div className="mb-4">
+            <label
+              htmlFor="otp"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Enter OTP:
+            </label>
+            <div className="flex justify-center">
+              {[...Array(6)].map((_, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  id={`otp-${index}`}
+                  className="w-10 h-10 p-2 border rounded-md text-center mx-1"
+                  maxLength="1"
+                  value={otp[index] || ""}
+                  onChange={(e) => handleOtpChange(e, index)}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {error && <p className="text-red-500 text-center">{error}</p>}
+        {!otpSent ? (
           <button
-            onClick={handleOtpVerification}
-            className={`bg-green-500 text-white py-2 px-4 rounded-md ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+            onClick={handleOtpSending}
+            className="w-full bg-gray-500 text-white py-2 px-4 rounded-md flex justify-center items-center mt-2"
             disabled={loading}
           >
-            {loading ? "Verifying OTP..." : "Verify OTP"}
+            {loading ? <CircularProgress size={24} /> : "Continue"}
           </button>
-          <button
-            onClick={handleResendOtp}
-            className={`bg-yellow-500 text-white py-2 px-4 rounded-md ml-2 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-            disabled={loading}
-          >
-            {loading ? "Resending OTP..." : "Resend OTP"}
+        ) : (
+          <div>
+            <button
+              onClick={handleOtpVerification}
+              className="w-full bg-green-500 text-white py-2 px-4 rounded-md flex justify-center items-center mt-2"
+              disabled={loading}
+            >
+              {loading ? <CircularProgress size={24} /> : "Verify OTP"}
+            </button>
+            <button
+              onClick={handleResendOtp}
+              className="w-full bg-yellow-500 text-white py-2 px-4 rounded-md mt-2 flex justify-center items-center"
+              disabled={loading}
+            >
+              {loading ? <CircularProgress size={24} /> : "Resend OTP"}
+            </button>
+          </div>
+        )}
+        <div className="my-4">
+          <div className="flex items-center justify-center">
+            <span className="h-px w-full bg-gray-300"></span>
+            <span className="px-3 text-gray-500">Or Continue With</span>
+            <span className="h-px w-full bg-gray-300"></span>
+          </div>
+          <button className="w-full bg-gray-200 text-black py-2 px-4 rounded-md flex justify-center items-center mt-2">
+            <FaGoogle className="mr-2" /> Google
           </button>
         </div>
-      )}
+      </div>
     </div>
   );
 };
