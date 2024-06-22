bc556be1cf9d2fb80d94080eb3f8a4d7181b12ac
SAEED NM <saeedmoozhikkal@gmail.com>
2024-06-22 19:37:18 +0530

    modification no 1 on the searchbar listing

--- room-booking/room-booking/src/services/searchServices.jsx
+++ room-booking/room-booking/src/services/searchServices.jsx
@@ -5,10 +5,10 @@ import instance from "../utils/Axiox";
 export const searchRooms = async (serachParams) =>{
     try {
         const params = new URLSearchParams(serachParams).toString();
-        {console.log('para ms is :',params);}
+        {console.log('param are   :',params);}
         const response = await instance.get(`accounts/room-search/?${params}`);
         {console.log('====================================');
-        console.log('data for fetching query from backend   ',response.data.results);
+        console.log('data  fetching  from backend   ',response.data.results);
         console.log('====================================');}
         return response.data.results;
     } catch (error) {
