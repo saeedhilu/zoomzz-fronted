b9369059788c01a0d3d31dcacb3281e9ddadd757
SAEED NM <saeedmoozhikkal@gmail.com>
2024-06-27 21:33:09 +0530

    updation

--- room-booking/room-booking/src/components/rooms/ImageGallery.jsx
+++ room-booking/room-booking/src/components/rooms/ImageGallery.jsx
@@ -11,29 +11,31 @@ const ImageGallery = ({ mainImage, otherImages }) => {
 
   return (
     <div className="flex flex-wrap gap-4">
-      <div className="w-1/3">
-        <img src={mainImage} alt="Main" className="w-full h-64 object-cover" />
-      </div>
-      <div className="w-1/3 flex flex-col gap-4">
-        {otherImages.slice(0, 2).map((image, index) => (
-          <img key={index} src={image} alt={`Other ${index + 1}`} className="w-full h-32 object-cover" />
-        ))}
-        {!showAll && otherImages.length > 2 && (
-          <div 
-            className="w-full h-32 bg-gray-200 flex items-center justify-center text-gray-500 text-lg cursor-pointer"
-            onClick={handleShowAllClick}
-          >
-            +{otherImages.length - 2} More Photos
+      {!showAll ? (
+        <>
+          <div className="w-1/2">
+            <img src={mainImage} alt="Main" className="w-full h-64 object-cover" />
           </div>
-        )}
-      </div>
-
-      {showAll && (
+          <div className="w-1/2 flex flex-col gap-4">
+            {otherImages.slice(0, 2).map((image, index) => (
+              <img key={index} src={image} alt={`Other ${index + 1}`} className="w-full h-32 object-cover" />
+            ))}
+            {otherImages.length > 2 && (
+              <div
+                className="w-full h-32 bg-gray-200 flex items-center justify-center text-gray-500 text-lg cursor-pointer"
+                onClick={handleShowAllClick}
+              >
+                +{otherImages.length - 2} More Photos
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
         <div className="w-full mt-4">
           <Carousel showThumbs={false} dynamicHeight={true} infiniteLoop={true} useKeyboardArrows={true}>
-            {otherImages.map((image, index) => (
+            {[mainImage, ...otherImages].map((image, index) => (
               <div key={index}>
-                <img src={image} alt={`Other ${index + 1}`} className="w-full h-64 object-cover" />
+                <img src={image} alt={`Carousel ${index + 1}`} className="w-full h-64 object-cover" />
               </div>
             ))}
           </Carousel>
