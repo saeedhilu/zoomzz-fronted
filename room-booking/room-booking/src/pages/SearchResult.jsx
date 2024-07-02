94d5f9064fc2cf5002ec1481dc0fe3a6e1a044e1
SAEED NM <saeedmoozhikkal@gmail.com>
2024-07-02 19:47:07 +0530

    updation

--- room-booking/room-booking/src/pages/SearchResult.jsx
+++ room-booking/room-booking/src/pages/SearchResult.jsx
@@ -15,8 +15,7 @@ const SearchResults = () => {
     <div className="max-w-6xl mx-auto p-4">
       <RoomSearch/>
       <h1 className="text-3xl font-bold mb-4">Search Results</h1>
-      <p className="bg-red-400 w-4"></p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+ <span className="absolute bottom-0 top-11 left-0 w-full h-1  bg-gray-500"></span>      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {rooms.length > 0 ? (
           rooms.map((room) => (
             <Link key={room.id} to={`/room-details/${room.id}`}>
