ae1dd7fbf5b8375c1b76cfb3886d39a0e9c30ec8
SAEED NM <saeedmoozhikkal@gmail.com>
2024-07-02 19:44:56 +0530

    updation

--- room-booking/room-booking/src/pages/SearchResult.jsx
+++ room-booking/room-booking/src/pages/SearchResult.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import { renderStars } from "../utils/ratingStar"; 
 import { Link } from "react-router-dom"; 
+import RoomSearch from "../components/layout/SearchBar";
 const SearchResults = () => {
   const location = useLocation();
   const { rooms } = location.state || { rooms: [] };
@@ -12,6 +13,7 @@ const SearchResults = () => {
   
   return (
     <div className="max-w-6xl mx-auto p-4">
+      <RoomSearch/>
       <h1 className="text-3xl font-bold mb-4">Search Results</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {rooms.length > 0 ? (
