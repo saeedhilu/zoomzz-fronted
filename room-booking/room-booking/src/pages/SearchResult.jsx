d8e658ea5da9b935d2405c3fbd26265039940182
SAEED NM <saeedmoozhikkal@gmail.com>
2024-07-02 19:47:50 +0530

    updation

--- room-booking/room-booking/src/pages/SearchResult.jsx
+++ room-booking/room-booking/src/pages/SearchResult.jsx
@@ -14,7 +14,7 @@ const SearchResults = () => {
   return (
     <div className="max-w-6xl mx-auto p-4">
       <RoomSearch/>
-      <h1 className="text-3xl font-bold mb-4">Search Results</h1>
+      <h1 className="text-3xl font-bold mb-4">Search Results </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         
         {rooms.length > 0 ? (
